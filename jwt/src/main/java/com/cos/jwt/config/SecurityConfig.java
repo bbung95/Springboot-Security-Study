813377b32a32df95604b4f61814ba6151af3f17c
bbung95 <tjd123123@naver.com>
2022-04-06 01:14:13 +0900

    feat : - Security Filter 설정 , Security의 filter는 작성한 filter보다 우선적으로 실행된다.

--- jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
+++ jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.cos.jwt.config;
 
+import com.cos.jwt.filter.MyFilter1;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.web.filter.CorsFilter;
 
 @Configuration
@@ -34,9 +36,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .anyRequest().permitAll();
 //////////////
 
+        //http.addFilterBefore(new MyFilter1(), BasicAuthenticationFilter.class);
+        //security에 filter등록, BasicAuthenticationFilter 전에 실행 (Spring의 필터종류를 알아야함)
+        //security의 필터는 Myfilter보다 무조건적으로 우선실행된다.
+
         http.csrf().disable();
         http.sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // session 방식 사용안함 //
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // session 방식 사용안함
                 .and()
                 .addFilter(corsFilter) // 모든 요청은 filter를 거치게된다. Controller에 @CrossOrigin(인증X), 시큐리티 핉터에 등록 인증(O)
                 .formLogin().disable() // form login 사용안함
